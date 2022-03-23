17f9dffc7f3481dde3c94f920232ef861ee56c26
zhouchao <1342101893@qq.com>
2022-03-23 18:03:21 +0800

    1.新增swing和javafx模块2.更新gitignore文件，忽略target文件夹

---
+++ swing-demo/src/main/java/com/zc/gui/LoginView.java
@@ -0,0 +1,12 @@
+package com.zc.gui;
+
+import javax.swing.*;
+
+public class LoginView {
+    private JPanel panel;
+    private JPanel panel1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
